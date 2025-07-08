21e6a01ec7cd54b76fbf08dd3fe14522b51920ea
Connors92 <connorgronsky@yahoo.com>
2025-07-08 08:41:24 -0700

    Asked Username

--- start/CH02/HelloWorld.py
+++ start/CH02/HelloWorld.py
@@ -2,4 +2,8 @@
 # A simple "Hello World" script in python
 # Created by Connor on 7/8/2025
 
-print("Hello World")
+#Get User Name
+user_name = input("What is your name? ")
+
+#Say hello to user
+print("Hello " + user_name)
