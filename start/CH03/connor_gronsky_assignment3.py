488138cbdca513a38e696175b1ef4248820c2823
Connors92 <connorgronsky@yahoo.com>
2025-07-09 09:31:17 -0700

    Loop today is good day

---
+++ start/CH03/connor_gronsky_assignment3.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# example working with conditionals
+#By Connor Gronsky on 7/9/2025
+
+#Asks user if today is good day
+answer = input("Is today a good day? (y/n): ")
+
+#Confirms if answer is 'y'
+if answer == 'y':
+
+    #Use loop to print answer 10 times
+         for x in range(10):
+            print("Yeah it is")
