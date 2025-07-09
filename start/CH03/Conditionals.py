bb976cd92e79032755f6a0e7e393fab8bcd188da
Connors92 <connorgronsky@yahoo.com>
2025-07-09 09:14:29 -0700

    Today good day confirmation

--- start/CH03/Conditionals.py
+++ start/CH03/Conditionals.py
@@ -1,3 +1,10 @@
 #!/usr/bin/env python3
-# example workign with conditionals
-#By 
+# example working with conditionals
+#By Connor Gronsky on 7/9/2025
+
+#Asks user if today is good day
+answer = input("Is today a good day? (y/n): ")
+
+#Confirms if answer is 'y'
+if answer == 'y':
+    print("Yes it is")
--- start/CH03/Loopy.py
+++ start/CH03/Loopy.py
@@ -1,3 +1,5 @@
 #!/usr/bin/env python3
 # example workign with Loops
-#By 
+#By Connor on 7/8/2025
+for x in range( 10 ):
+    print ("Hello World")
