aa885714ea3eb754b4366d6cd44df51b4c36621c
Connors92 <connorgronsky@yahoo.com>
2025-07-29 09:36:24 -0700

    Brute Force Attack

--- start/CH06/bruteforceAttack.py
+++ start/CH06/bruteforceAttack.py
@@ -1,11 +1,23 @@
 #!/usr/bin/env python3
 # Example brute-force attacker
-#By Ed Goad
-# date: 2/5/2021
+#By Connor Gronsky on 7/29/2025
 
 # NOTE: this example is limited to numbers. There are no letters or symbols in this example
 # Suggested to start by debugging to show how brute force walks through all available options 
 
+from passlib.hash import sha512_crypt
 
-hashed_password = "$6$G.DTW7g9s5U7KYf5$QFcHx0/J88HV/Q0ab653gfYQ1KyNGx5HRhDQYyai2ZUy7Aw4tyfJ6/kI6kllfXl0DyS.LuaUJvqnlIn2fVM5F0"
-algorithm_salt = "$6$G.DTW7g9s5U7KYf5$"
+def test_password(hashed_password, salt, plaintext_password):
+    crypted_password = sha512_crypt.using(rounds=5000).hash(plaintext_password, salt=salt)
+    return hashed_password == crypted_password
+
+hashed_password = "$6$G.DTW7g9s5U7KYf5$QFcHx0/J88HV/Q0ab653"
+hashed_password += "gfYQ1KyNGx5HRhDQYyai2ZUy7Aw4tyfJ6/kI6kl"
+hashed_password += "lfXl0DyS.LuaUJvqnlIn2fVM5F0"
+salt = "G.DTW7g9s5U7KYf5"
+
+for password in range(100000):
+    result = test_password(hashed_password, salt, str(password))
+    if result:
+        print("Match found: {0}".format(password))
+        break
