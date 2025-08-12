364d61ae697d0c1963f2a1d4dea19ca1a1c11ff2
Connors92 <connorgronsky@yahoo.com>
2025-08-12 08:09:44 -0700

    Updated people in space

--- start/CH08/PeopleInSpace2.py
+++ start/CH08/PeopleInSpace2.py
@@ -1,3 +1,21 @@
 #!/usr/bin/env python3
 # Script that tells us how many people there are in space
-#By 
+# By Connor Gronsky on 8/12/2025
+
+# Import Python modules
+import requests
+import json
+
+def get_people_in_space():
+    request_uri = "http://api.open-notify.org/astros.json"
+    r = requests.get(request_uri)
+    items = r.json()
+    return items
+
+astronauts = get_people_in_space()
+
+# Print basic details
+print(astronauts)
+
+# Print "pretty" JSON
+print(json.dumps(astronauts, indent=2))
